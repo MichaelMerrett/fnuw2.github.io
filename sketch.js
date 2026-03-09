b9d93c811134edb3131b9f2a7f72ede7bd8400aa
Michael Merrett <127238052+MichaelMerrett@users.noreply.github.com>
2026-03-09 11:01:31 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -131,9 +131,9 @@ let jumpscareShakeTimer = 0;
 let jumpscareBurstFired = false;
 
 //all
-let width = 1920;
+let width = 2560;
 let mainWidth = width * 1.2953125; // so max translate is 567 at 1080p
-let height = 1080;
+let height = 1440;
 let mainFont;
 
 //characters
@@ -838,7 +838,7 @@ michael.tick = function() {
         michael.timeSinceLastFlickerChance += deltaTime;
         if (michael.timeSinceLastFlickerChance >= 5000) {
             michael.timeSinceLastFlickerChance = 0;
-            if (random() < michael.ai / 100) {
+            if (random() < michael.ai / 40) {
                 michael.update();
                 if (level == "main") {
                     flickerTimer = 500;
@@ -1024,6 +1024,7 @@ function preload() {
     mainFont = loadFont("assets/VCR_OSD_MONO.ttf", assetLoaded);
     debugLog("Loading images...")
     cameraMap = loadImageWrapper("assets/images/cameras.png");
+    computerView = loadImageWrapper("assets/images/pc_done.png")
 
     // load the cams
     abdullahCam[0] = loadImageWrapper("assets/images/abdullah/frame0.webp");
