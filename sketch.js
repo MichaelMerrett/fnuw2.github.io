0787ce484fad6c0bbbc76c2c0e5559ed03370548
Michael Merrett <127238052+MichaelMerrett@users.noreply.github.com>
2026-03-11 13:33:52 -0400

    Update width and height to use window dimensions

--- sketch.js
+++ sketch.js
@@ -131,9 +131,9 @@ let jumpscareShakeTimer = 0;
 let jumpscareBurstFired = false;
 
 //all
-let width = 1920;
+let width = window.innerWidth;
 let mainWidth = width * 1.2953125; // so max translate is 567 at 1080p
-let height = 1080;
+let height = window.innerHeight;
 let mainFont;
 
 //characters
